371a970c8c068f40b93e522c98df0690dc540b02
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-04-13 14:06:56 +0100

    Add documentation, prepare for the first pre-release.

--- TerminalPass/Properties/AssemblyInfo.cs
+++ TerminalPass/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TerminalPass")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An open source password manager for your terminal.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("foreggs")]
 [assembly: AssemblyProduct("TerminalPass")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
---
+++ TerminalPass/README.md
@@ -0,0 +1,6 @@
+﻿# Terminal Pass
+**Terminal Pass** is an open source, terminal based password manager written in C#.
+It uses the latest **Aes 256** encryption and stores your passwords in your user folder (C:\Users\{{user}}\terminal-pass.txt).
+
+# Getting started
+Firstly, you need to [download](https://github.com/foreggs/terminal-pass/) one of the official releases or clone this repo and build it yourself.
