7332b9fd01b40c874fac7c2f8844b0cc1bf14e25
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-04-13 13:47:27 +0100

    Add project files.

---
+++ TerminalPass/Aes.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalPass
+{
+    class Aes
+    {
+        public static byte[] defaultSalt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        public static string Encrypt(string toEncrypt, string password)
+        {
+            byte[] bytesToBeEncrypted = Encoding.ASCII.GetBytes(toEncrypt);
+            byte[] passwordBytes = Encoding.ASCII.GetBytes(password);
+
+            byte[] encryptedBytes = null;
+            byte[] saltBytes = defaultSalt;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+            return BitConverter.ToString(encryptedBytes).Replace("-", "");
+        }
+
+        public static string Decrypt(string hexToDecrypt, string password)
+        {
+            byte[] bytesToBeDecrypted = StringToByteArray(hexToDecrypt);
+            byte[] passwordBytes = Encoding.ASCII.GetBytes(password);
+
+            byte[] decryptedBytes = null;
+            byte[] saltBytes = defaultSalt;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return Encoding.ASCII.GetString(decryptedBytes);
+        }
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
---
+++ TerminalPass/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+using System.IO;
+using Newtonsoft.Json;
+using System.Security;
+using ConsoleTables;
+using Alba.CsConsoleFormat.Fluent;
+
+namespace TerminalPass
+{
+    class AccountRecord
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
+    class Configuration
+    {
+        public bool Hide { get; set; }
+        public Format TableFormat { get; set; } = Format.Alternative;
+        public AccountRecord[] Accounts { get; set; } = new AccountRecord[] { };
+    }
+
+    class Program
+    {
+        public static string path = Environment.OSVersion.Version.Major >= 6
+            ? Directory.GetParent(Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName).ToString()
+            : Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+        public static string fileName = "terminal-pass.txt";
+        public static string absolutePath = Path.Combine(path, fileName);
+
+        public static string masterPassword;
+        public static Configuration config;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                try
+                {
+                    using (var r = new StreamReader(absolutePath))
+                    {
+                        Console.Write("Password: ");
+                        masterPassword = GetPassword();
+                        Console.Clear();
+
+                        string encoded = r.ReadToEnd();
+                        string json = Aes.Decrypt(encoded, masterPassword);
+                        config = JsonConvert.DeserializeObject<Configuration>(json);
+                    }
+                    EditConfig();
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("==========NEW LOGIN, CREATE NEW FILE==========");
+                    Console.Write("Password: ");
+                    string masterPassword = GetPassword();
+                    Console.WriteLine();
+
+                    Configuration config = new Configuration
+                    {
+                        Accounts = new[] {
+                        new AccountRecord { Name = "Google", Password ="CanAamishPlease" },
+                        new AccountRecord { Name = "Outlook", Password = "GetAnArrangedMarriange" },
+                        new AccountRecord { Name = "Github", Password = "OrIWillShowHimDaWae" },
+                    },
+                    };
+
+                    WriteConfig(config, masterPassword);
+                }
+                catch (JsonReaderException)
+                {
+                    Console.WriteLine("Error parsing the file");
+                }
+                catch (CryptographicException)
+                {
+                    Console.WriteLine("Invalid file or password");
+                }
+
+                // [x] if file not exists -> create one -> ask to create password
+                // else -> while password not valid -> ask for password; allow to:
+                //    view, add, delete rows
+                //    search
+            }
+        }
+
+        public static void EditConfig()
+        {
+            string c = "";
+            while(c != "exit")
+            {
+                var accounts = new List<AccountRecord>();
+                for (int i = 0; i < config.Accounts.Length; i++)
+                {
+                    var acc = new AccountRecord{
+                        Name = config.Accounts[i].Name,
+                        Password = config.Hide ? new string('*', config.Accounts[i].Password.Length) : config.Accounts[i].Password
+                    };
+                    accounts.Add(acc);
+
+                }
+                if (c != "help")
+                    ConsoleTable.From(accounts).Write((Format)config.TableFormat);
+
+                Console.Write("terminal-pass> ");
+                string raw = Console.ReadLine();
+                string[] tokens = raw.Split(' ').ToArray();
+                try
+                {
+                    c = tokens[0];
+                    switch (c)
+                    {
+                        case "help":
+                            Colors.WriteLine("\treload".Yellow(), " -> ", "Updates the file and reloads the table");
+                            Colors.WriteLine("\tdelete".Yellow(), "name".Green(), " -> ", "Deletes row with that name");
+                            break;
+                        case "reload":
+                            WriteConfig(config, masterPassword);
+                            Console.Clear();
+                            break;
+                        // delete name
+                        case "delete":
+                            string nameToDelete = tokens[1];
+                            config.Accounts = config.Accounts.Where(acc => acc.Name != nameToDelete).ToArray();
+                            WriteConfig(config, masterPassword);
+                            Console.Clear();
+                            break;
+                        // add name password
+                        case "add":
+                            string name = tokens[1];
+                            string password = tokens[2];
+                            config.Accounts = config.Accounts
+                                .Concat(new AccountRecord[] { new AccountRecord { Name = name, Password = password } })
+                                .ToArray();
+                            WriteConfig(config, masterPassword);
+                            Console.Clear();
+                            break;
+                        case "set":
+                            switch(tokens[1])
+                            {
+                                case "hide":
+                                    if (bool.TryParse(tokens[2], out bool hide))
+                                    {
+                                        config.Hide = hide;
+                                    }
+                                    break;
+                            }
+                            WriteConfig(config, masterPassword);
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine($"Invalid name {tokens[1]}.");
+                            break;
+                    }
+                } catch(Exception e)
+                {
+                    Console.WriteLine("Invalid command");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public static string GetPassword()
+        {
+            string pwd = "";
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd = pwd.Remove(pwd.Length - 1, 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else
+                {
+                    pwd += i.KeyChar;
+                    Console.Write("*");
+                }
+            }
+            return pwd;
+        }
+
+        public static void WriteConfig(Configuration config, string password)
+        {
+            string configString = JsonConvert.SerializeObject(config, Formatting.Indented);
+            string encryptedConfigString = Aes.Encrypt(configString, password);
+            File.WriteAllText(absolutePath, encryptedConfigString);
+        }
+    }
+}
